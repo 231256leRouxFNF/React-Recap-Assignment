e78c646bc908a7bc148c26135d9657f971e37722
KeaganCB-OW <160649882+KeaganCB-OW@users.noreply.github.com>
2025-05-08 10:22:06 +0200

    About and homePage start

--- consult-park/src/App.js
+++ consult-park/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage';
 import ServicesPage from "./pages/ServicesPage";
 import BPOPage from "./pages/BPOPage";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
+      <Route path="/" element={<HomePage />} />
         <Route path="/services" element={<ServicesPage />} />
         <Route path="/bpo" element={<BPOPage />} />
         {/* Add other routes like Home, About, Contact here */}
